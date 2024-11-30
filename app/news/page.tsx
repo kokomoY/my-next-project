56b64072e72a1c23521085723e742de97c19e8d4
kokomoY <ktc24a31e0017@edu.kyoto-tech.ac.jp>
2024-11-30 16:28:34 +0900

    commit

---
+++ app/news/page.tsx
@@ -0,0 +1,8 @@
+import { Props } from "next/script";
+import { getNewsList } from "../_libs/microcms";
+
+export default async function Page() {
+    const { contents: news } = await getNewsList();
+
+    return <div>{JSON.stringify(news)}</div>;
+}
