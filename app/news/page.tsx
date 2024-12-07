d08c57bf0eaabeb0338a8ecf1cef65fabfe5fcd4
kokomoY <ktc24a31e0017@edu.kyoto-tech.ac.jp>
2024-12-07 16:26:59 +0900

    commit

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,11 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewList from "@/app/_components/NewList";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 export default async function Page() {
-    const { contents: news } = await getNewsList();
+    const { contents: news } = await getNewsList({
+        limit: NEWS_LIST_LIMIT,
+    });
 
     return <NewList news={news} />;
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 import { getNewsList } from "@/app/_libs/microcms";
-import { TOP_NEWS__LIMIT }  from "./constants";
+import { TOP_NEWS__LIMIT } from "./_constants";
 import NewList from "@/app/_components/NewList";
-import ButtonLink from "@/app/_components/ButtonLink"  
+import ButtonLink from "@/app/_components/ButtonLink";
 
 export default async function Home() {
   const data = await getNewsList({
@@ -26,7 +26,6 @@ export default async function Home() {
           width={4000}
           height={1200}
         />
-      
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
@@ -38,4 +37,3 @@ export default async function Home() {
     </>
   );
 }
-
