a46d53f49b1ad6d45709ad855635867c1b9503ab
kokomoY <ktc24a31e0017@edu.kyoto-tech.ac.jp>
2024-12-07 14:46:40 +0900

    commit

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,9 @@
-import { Props } from "next/script";
-import { getNewsList } from "../_libs/microcms";
+import { getNewsList } from "@/app/_libs/microcms";
+import NewList from "@/app/_components/NewList";
 
 export default async function Page() {
     const { contents: news } = await getNewsList();
 
-    return <div>{JSON.stringify(news)}</div>;
+    return <NewList news={news} />;
+
 }
